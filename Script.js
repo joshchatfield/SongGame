f6b1fb4ccb42794a7af93e89ca6e8268c203673d
Josh Chatfield <joshchatfield@gmail.com>
2022-03-05 10:56:12 -0800

    update

--- Script.js
+++ Script.js
@@ -7,7 +7,6 @@ var Genre = "";
 var DisplayedSongTitle = "";
 
 function MapData (data) {
-    console.log("mapdata triggered")
     Song = "Under The Table And Dreaming";
     Artist = "Dave Matthews";
     Album = "Under The Table And Dreaming";
@@ -16,42 +15,40 @@ function MapData (data) {
 }
 
 function Guess () {
-    console.log("guessed");
 }
 
 function ShowArtist () {
     var el = document.getElementById("artist");
-    el.style.display = "inline"
+    el.innerHTML = Artist;
+}
+
+function ReplaceCharAtStrIndex (index, char, string) {
+    return string.substr(0, index) + char + string.substr(index + 1);
 }
 
 function ShowRandomizedLetters () {
-    var el = document.getElementById("artist");
+    var el = document.getElementById("randomized-letters");
+    var SongTemp = Song.toUpperCase();
 
-    var SongTemp = Song;
-
-    for (var i = 0; i < Song.length; i++){
-        
-        var random = 5; //random num between 0 and song.length - 1
+    for (var i = 0; i < SongTemp.length; i++){
+        var random = Math.floor(Math.random() * SongTemp.length);; 
         var temp1 = SongTemp[i];
         var temp2 = SongTemp[random];
 
-        SongTemp[i] = temp2;
-        SongTemp[random] = temp1;
-
-
+        SongTemp = ReplaceCharAtStrIndex(i, temp2, SongTemp);
+        SongTemp = ReplaceCharAtStrIndex(random, temp1, SongTemp);
     }
-
-    el.innerHTML = DisplayedSongTitle;
+    el.innerHTML = SongTemp;
 }
 
 function ShowAlbum () {
     var el = document.getElementById("album");
-    el.style.display = "inline"
+    el.innerHTML = Album;
 }
 
 function ShowReleaseYear () {
     var el = document.getElementById("release-year");
-    el.style.display = "inline"
+    el.innerHTML = ReleaseYear;
 }
 
 function UpdateDisplayedSongTitle () {
@@ -59,8 +56,31 @@ function UpdateDisplayedSongTitle () {
     el.innerHTML = DisplayedSongTitle;
 }
 
-//actions
-function OnLoad (){
-    console.log("OnLoadTriggered");
+function ShowWordStructure () {
+
+}
+
+function SetupSongTitle () {    
+    if(Song.length > 37){
+        //TODO: start new game until length is not > 35
+    } else {
+        el = document.getElementById("song-title");
+        var html = '<table class = "song-table"><tr>';
+
+        for (var i = 0; i < Song.length; i++) {
+            html += '<td>' + Song[i] + '</td>';
+        }
+    
+        html += "</tr></table>"
+        console.log(html);
+        el.innerHTML = html;
+    }
+}
+
+function StartNewGame () {
     MapData();
+    SetupSongTitle();
+}
+
+function OnLoad (){
 }
