daad0efe32e6a7f7a7ace14a01af495d32cdbfc7
Josh Chatfield <joshchatfield@gmail.com>
2022-03-22 12:50:18 -0700

    trying to allow get script.js

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const port = 8080;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/script.js', (req, res) => {
+  res.sendFile(__dirname + '/public/script.js');
 });
 
 app.listen(port, () => {
